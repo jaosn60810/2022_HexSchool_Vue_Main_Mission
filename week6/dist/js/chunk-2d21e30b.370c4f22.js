b4b6bde96ca8b2970adb4a62e93fe0c9ad6c676d
jason60810 <jason60810@gmail.com>
2022-02-20 19:36:35 +0800

    chore: 建造 dist

--- week6/dist/js/chunk-2d21e30b.370c4f22.js
+++
@@ -1,2 +0,0 @@
-(window["webpackJsonp"]=window["webpackJsonp"]||[]).push([["chunk-2d21e30b"],{d504:function(n,e,t){"use strict";t.r(e);var c=t("7a23");function d(n,e,t,d,r,u){return Object(c["y"])(),Object(c["g"])("div",null,"首頁")}var r={name:"Index",data:function(){return{}}},u=t("6b0d"),a=t.n(u);const o=a()(r,[["render",d]]);e["default"]=o}}]);
-//# sourceMappingURL=chunk-2d21e30b.370c4f22.js.map
